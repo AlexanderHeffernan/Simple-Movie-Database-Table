b14f6619f49512d4db2c6a6b02c3127dc3b3f575
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-05-10 09:41:26 +1200

    Started working on VectorDbTable

--- vectordb.hpp
+++ vectordb.hpp
@@ -7,3 +7,37 @@
  * as specified in the hand-out (Task 2)
  */ 
 
+ namespace nwen {
+    class VectorDbTable : public AbstractDbTable {
+    private:
+        std::vector<movie> database;
+
+    public:
+        int rows() const override {
+            return database.size();
+        }
+
+        const movie* get(int index) const override {
+            if (index >= 0 && index < database.size()) {
+                return &database[index];
+            } else {
+                return nullptr;
+            }
+        }
+
+        void add(const movie& movie) override {
+            database.push_back(movie);
+        }
+
+        bool update(unsigned long id, const movie& movie) override {
+            for (auto& entry : database) {
+                if (entry.id == id) {
+                    entry = movie;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+ }
+
